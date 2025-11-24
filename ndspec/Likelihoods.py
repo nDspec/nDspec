83a79bdc4b3d219aef590e52320719d07de01153
Matteo Lucchini <m.lucchini@uva.nl>
2025-11-24 15:41:14 +0100

    Included docstrings in likelihood functions

--- ndspec/Likelihoods.py
+++ ndspec/Likelihoods.py
@@ -2,8 +2,44 @@ import numpy as np
 from .SimpleFit import SimpleFit as simple
 
 def delchi(data,err,model,noise=None,noise_err=None,residuals=False):
+    """
+    This function is used to calculate the traditional chi-squared statistic 
+    drawn from Gaussian-distributed data. It also accounts for a background/noise 
+    term, by subtracting the (optional) user-provided noise from the data, and 
+    summing the uncertainties of the noise and data in quadrature. 
+    
+    Parameters:
+    -----------
+    data: np.array(float)
+        The value of datapoints from which to calculate the statistic 
+    
+    err: np.array(float)
+        The uncertainties on each data point 
+    
+    model: np.array(float)
+        The model values to be compared to the data in the statistic 
+    
+    noise: np.array(float), default None
+        The optional background values to be subtracted from the data when 
+        calculating the statistic. If it is not specified, no subtraction is 
+        performed
+        
+    noise_err: np.array(float), default None
+        The optional background uncertainty values to be added in quadrature to 
+        the error on the data. If it is not specified, no uncertainty is added.
+        
+    residuals: bool, default False 
+        A boolean to either return the summed statistic (False) or an array with 
+        the contribution of each bin to the total statistic (True)
+        
+    Returns:
+    --------
+    delchi: np.array(float)
+        Either a single value containing the summed statistic (if residuals is 
+        True), or an array containing the contribution of each bin to the total 
+        statistic (if residuals is False).
+    """
 
-    #check that both noise and noise_err are present, or absent
     if (noise is None and noise_err is not None):
         raise ValueError("The background is not defined but its error is")
     if (noise is not None and noise_err is None):
@@ -20,7 +56,52 @@ def delchi(data,err,model,noise=None,noise_err=None,residuals=False):
     elif residuals is True:
         return delchi
     
-def ratio(data,err,model,noise=None,noise_err=None,residuals=True,bars=True):
+def ratio(data,err,model,noise=None,noise_err=None,summed=True,bars=True):
+    """
+    This function is used to calculate the ratio of the data to a model. If a 
+    background and its uncertainty are provided, these are subtracted from the 
+    data and summed in quadrature to the error, respectively.
+    
+    Parameters:
+    -----------
+    Parameters:
+    -----------
+    data: np.array(float)
+        The value of datapoints from which to calculate the ratio 
+    
+    err: np.array(float)
+        The uncertainties on each data point 
+    
+    model: np.array(float)
+        The model values to be compared to the data in the ratio 
+    
+    noise: np.array(float), default None
+        The optional background values to be subtracted from the data when 
+        calculating the ratio. If it is not specified, no subtraction is 
+        performed
+        
+    noise_err: np.array(float), default None
+        The optional background uncertainty values to be added in quadrature to 
+        the error on the data. If it is not specified, no uncertainty is added.
+        
+    summed: bool, default False 
+        A boolean to either return the summed ratios (False) or an array with 
+        the ratio in each bin to the total statistic (True)
+        
+    bars: bool, default True 
+        A boolean to control whether to return both an array with the ratios and 
+        one with the corresponding error bars (True), or just the ratios (False).
+        
+    Returns:
+    --------
+    ratio: np.array(float)
+        Either a single value containing the summed ratios (if summed is 
+        True), or an array containing the ratio in each bin (if summed is False).
+        
+    bars: np.array(float) 
+        An array containing the error bars of the data normalized by the model. 
+        Used for ratio plots.
+    """
 
     if noise is not None:
         data = data-noise 
@@ -36,7 +117,54 @@ def ratio(data,err,model,noise=None,noise_err=None,residuals=True,bars=True):
         return ratio
 
 def cstat(data,model,exp,widths,noise=None,residuals=False):
-       
+    """
+    This function calculates the Cash statistic (Cash 1979, DOI: 10.1086/156922)
+    for Poisson-distributed count data (typically, a time-averaged X-ray 
+    spectrum). Optionally, users can also provide a background count rate, which 
+    is also assumed to be Poissoni-distributed. The implementation is identical 
+    to that of Xspec, with the additional assumption that the exposure times for 
+    the background and data are identical. For more details of the math used, 
+    see the Xspec documentation:
+    https://heasarc.gsfc.nasa.gov/docs/software/xspec/manual/node340.html
+    
+    Parameters:
+    -----------
+    data: np.array(float)
+        The data for which to calculate the statistic. This MUST be provided in 
+        units of counts/s/keV (e.g., identical to the array stored in a ndspec 
+        FitTimeAvgSpectrum object).
+        
+    model: np.array(float)
+        The model values for which to calculate the statistic. This MUST be 
+        provided in units of counts/s/keV (e.g., identical to the array 
+        calculated by the eval_model() method of a ndspec FitTimeAvgSpectrum
+        object).
+        
+    exp: float 
+        The exposure time (in seconds) of the data and background. 
+        
+    widths: np.array(float)
+        An array of channel widths, in units of keV; this can come, for example, 
+        from the difference between the "emax" and "emin" columns of a OGIP
+        compatible response matrix file.
+        
+    noise: np.array(float), default None
+        The background spectrum to be included in calculating the statistic; 
+        thiis MUST be provided in units of counts/s/keV (e.g., identical to the 
+        array stored in a ndspec FitTimeAvgSpectrum object).
+        
+    residuals: bool, default False 
+        A boolean to either return the summed statistic (False) or an array with 
+        the contribution of each bin to the total statistic (True)
+    
+    Returns:
+    --------
+    cstat: np.array(float)
+        Either a single value containing the summed statistic (if residuals is 
+        True), or an array containing the contribution of each bin to the total 
+        statistic (if residuals is False).        
+    """       
+    
     #fix the factor needed to convert to integrated counts, then calculate 
     #the model and data in integrated counts  
     conv_factor = exp*widths
